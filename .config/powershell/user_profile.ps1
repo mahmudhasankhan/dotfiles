1fd49f2d09ff8392dd999aa79d320ada1e3eb270
mahmudhasankhan <khanzadeh008@gmail.com>
2024-02-07 00:32:44 +0600

    Updated powershell readme & powershell profile config

--- .config/powershell/user_profile.ps1
+++ .config/powershell/user_profile.ps1
@@ -2,26 +2,33 @@
 function Get-ScriptDirectory { Split-Path $MyInvocation.ScriptName }
 $PROMPT_CONFIG = Join-Path (Get-ScriptDirectory) 'uew.omp.json'
 oh-my-posh init pwsh --config $PROMPT_CONFIG | Invoke-Expression
-
+#
 # PSReadline
 Set-PSReadlineOption -EditMode Windows
 Set-PSReadlineOption -BellStyle None
 Set-PSReadlineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
 Set-PSReadlineOption -PredictionSource History
-
-# Icons
+#
+# # Icons
 Import-Module -Name Terminal-Icons
-
-# Fzf
+#
+# # Fzf
 Import-Module PSFzf
 Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
-
-#Alias
-Set-Alias vim nvim
-Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
-Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
+#
+# #Alias
+# Set-Alias vim nvim
+# Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
+# Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
 
 # Alias for commands
 Set-Alias -Name touch -Value New-Item
 Set-Alias -Name pwd -Value Get-Location
-Set-Alias -Name mv -Value Move-Item
+Set-Alias -Name mv -Value Move-Item 
+Set-Alias -Name vim -Value nvim 
+
+# Utility command
+function which ($command) {
+	Get-Command -Name $command -ErrorAction SilentlyContinue |
+		Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
+}
