66ea1e166d5c21a5b870aac6e42c305032686490
Mahmud Hasan Khan <khanzadeh008@gmail.com>
2025-04-10 12:46:45 +0600

    added a few plugins

--- nvim/init.lua
+++ nvim/init.lua
@@ -8,7 +8,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   if vim.v.shell_error ~= 0 then
     error('Error cloning lazy.nvim:\n' .. out)
   end
-end 
+end
 vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup({
@@ -19,5 +19,11 @@ require('lazy').setup({
     require 'plugins.bufferline',
     require 'plugins.lualine',
     require 'plugins.treesitter',
-    require 'plugins.telescope'
+    require 'plugins.telescope',
+    require 'plugins.lsp',
+    require 'plugins.autocompletion',
+    require 'lua.plugins.gitsigns',
+    require 'lua.plugins.alpha',
+    require 'lua.plugins.indent-blankline',
+    require 'plugins.misc'
 })
